743b6364704a1a58b8fbb6daba1839fab9528377
jaures kouassi <jauresko@gmail.com>
2019-08-24 10:24:37 +0200

    travel model and controller

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,5 +1,5 @@
 class Travel < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :first_name, :last_name, :photo, :address, :nationality, :telephone, :whatsapp_number, presences: true
+  validates :first_name, :last_name, :address, :nationality, :telephone, :whatsapp_number, presence: true
 end
