24afe7babec77d1dad23337642e462fbc1f4dd72
jaures kouassi <jauresko@gmail.com>
2019-08-25 19:09:09 +0200

    validates for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :travel
-  valdiates :date, :meeting_place, :drop_place, :total_price, presence: true
+  validiates :date, :meeting_place, :drop_place, :total_price, presence: true
 end
