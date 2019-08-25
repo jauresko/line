16421a35c7957cb643cfa6e441df5adbc3c0b5a8
jaures kouassi <jauresko@gmail.com>
2019-08-25 19:13:15 +0200

    correction

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :travel
-  validiates :date, :meeting_place, :drop_place, :total_price, presence: true
+  validates :date, :meeting_place, :drop_place, :total_price, presence: true
 end
