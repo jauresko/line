f6313fb6043de17616bcd393eee6a8f466594781
jaures kouassi <jauresko@gmail.com>
2019-08-25 14:36:02 +0200

    model validation resolve error

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,5 +1,4 @@
 class Travel < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :first_name, :last_name, :address, :nationality, :telephone, :whatsapp_number, presence: true
 end
