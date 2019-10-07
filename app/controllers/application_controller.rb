9aa042040be46ce31a3ee42e6b58b1ecf5a260b8
jaures kouassi <jauresko@gmail.com>
2019-10-08 01:41:00 +0200

    I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,21 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_locale
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo, :address, :document,:nationality, :telephone, :whatsapp_number])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo, :document, :address, :nationality, :telephone, :whatsapp_number])
+  end
+
+  def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  resources :travels do
-    resources :bookings do
-      resources :chat_rooms, only: [:show] do
-        resources :messages, only: [:create]
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    resources :travels do
+      resources :bookings do
+        resources :chat_rooms, only: [:show] do
+          resources :messages, only: [:create]
+        end
+        resources :reviews, only: [:new, :create, :edit, :update]
       end
-      resources :reviews, only: [:new, :create, :edit, :update]
     end
-  end
 
-  resources :purchases do
-    resources :orders do
-      resources :chat_rooms, only: [:show] do
-        resources :messages, only: [:create]
+    resources :purchases do
+      resources :orders do
+        resources :chat_rooms, only: [:show] do
+          resources :messages, only: [:create]
+        end
+        resources :review_orders, only: [:new, :create, :edit, :update]
       end
-      resources :review_orders, only: [:new, :create, :edit, :update]
     end
+
+    get 'myprofile', to: 'pages#profil', as: 'profil'
+    get 'myprofile/mypurchase', to: 'purchases#mypurchase', as: 'mypurchase'
+    get 'myprofile/mytravel', to: 'travels#mytravel', as: 'mytravel'
+    get 'myprofile/order', to: 'orders#display_resa', as: 'myorder'
+    get 'myprofile/booking', to: 'bookings#display_resa', as: 'mybooking'
   end
-
-  get 'myprofile', to: 'pages#profil', as: 'profil'
-  get 'myprofile/mypurchase', to: 'purchases#mypurchase', as: 'mypurchase'
-  get 'myprofile/mytravel', to: 'travels#mytravel', as: 'mytravel'
-  get 'myprofile/order', to: 'orders#display_resa', as: 'myorder'
-  get 'myprofile/booking', to: 'bookings#display_resa', as: 'mybooking'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
