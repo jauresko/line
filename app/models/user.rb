278b307a191dfa02aef5219b0dc901a42330669e
jaures kouassi <jauresko@gmail.com>
2019-11-14 03:50:36 +0100

    facebook oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable, :confirmable
-  validates :first_name, :last_name, :address, :nationality, :telephone, :whatsapp_number, presence: true
+  devise :omniauthable, :omniauth_providers => [:facebook]
+  # validates :first_name, :last_name, :address, :nationality, :telephone, :whatsapp_number, presence: true
   has_many :travels
   has_many :bookings
   has_many :reviews
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name # assuming the user model has a name
+      user.image = auth.info.image
+      user.birthday = auth.info.birthday
+      user.address = auth.info.address
+      user.save# assuming the user model has an image
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,6 +19,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'jaures.kouassi.pro@gmail.com'
+  config.omniauth :facebook, "418982935409608", "6d7b221603e58315fec9875a5dc0b48b", info_fields: 'email, first_name, last_name, image, birthday, address', callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
