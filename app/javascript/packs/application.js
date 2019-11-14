06313b88f9a90042c2122e3939c346545369ccb6
jaures kouassi <jauresko@gmail.com>
2019-11-14 01:28:24 +0100

    show travel redesign

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,11 @@ import "bootstrap";
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import '../plugins/flatpickr'
 import {previewImageOnFileSelect} from "../plugins/photo_preview"
-import "../plugins/scroll"
+import "../plugins/scroll";
+
 
 initAutocomplete();
 previewImageOnFileSelect();
+
+
 
