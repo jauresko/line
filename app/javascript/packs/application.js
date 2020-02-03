d8eeaf5b93c0e9f539931fc2c4acd161f92ff3c7
jaures kouassi <jauresko@gmail.com>
2020-02-03 22:14:26 +0100

    garbage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,9 @@ import { initAutocomplete } from "../plugins/init_autocomplete";
 import '../plugins/flatpickr'
 import {previewImageOnFileSelect} from "../plugins/photo_preview"
 import "../plugins/scroll";
-import {animationJs} from "../plugins/animation_card";
+
 
 
 initAutocomplete();
 previewImageOnFileSelect();
-animationJs();
 
